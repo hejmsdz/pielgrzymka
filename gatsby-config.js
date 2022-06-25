48064543b7d9d49ed41bc094c12761189d4271ab
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2022-06-26 00:33:14 +0200

    add offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,24 +4,34 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`
     },
     plugins: [
-        "gatsby-plugin-sass", {
+        "gatsby-plugin-sass",
+        {
             resolve: 'gatsby-plugin-manifest',
             options: {
                 "icon": "src/images/icon.png"
             }
-        }, {
+        },
+        {
+            resolve: `gatsby-plugin-offline`,
+            options: {
+                precachePages: ['/dzien/*/*'],
+            },
+        },
+        {
             resolve: "gatsby-plugin-mdx",
             options: {
                 defaultLayouts: {
                     default: require.resolve('./src/components/layout.js'),
                 }
             },
-        }, {
+        },
+        {
             resolve: 'gatsby-source-filesystem',
             options: {
                 "name": "pages",
                 "path": "./src/pages/"
             },
             __key: "pages"
-        }]
+        },
+    ]
 };
