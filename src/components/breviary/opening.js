a00f3896780878f769be167bb54b9f79c1d86bf3
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2022-06-26 10:45:29 +0200

    day 7

--- src/components/breviary/opening.js
+++ src/components/breviary/opening.js
@@ -2,13 +2,13 @@ import React from 'react';
 import K from '../k'
 import W from '../w'
 
-export default () => (
+export default ({ alleluja = true }) => (
     <p>
         <K>† Boże, wejrzyj ku wspomożeniu memu.</K>
         <W>Panie, pośpiesz ku ratunkowi memu.</W>
         Chwała Ojcu i Synowi, i Duchowi Świętemu.
         <br />
         Jak była na początku, teraz i zawsze,
-        <br />i na wieki wieków. Amen. Alleluja.
+        <br />i na wieki wieków. Amen. {alleluja && 'Alleluja.'}
     </p>
 );
--- src/components/breviary/responsory.js
+++ src/components/breviary/responsory.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default ({ a, b, c }) => (
     <section>
         <h2>Responsorium krótkie</h2>
-        <div class="k">{a} * {b}</div>
-        <div class="w">{a} / {b}</div>
+        <div class="k">{a}{b && ` * ${b}`}</div>
+        <div class="w">{a}{b && ` / ${b}`}</div>
         <div class="k">{c}</div>
-        <div class="w">{b}</div>
+        <div class="w">{b || a}</div>
         <div class="k">Chwała Ojcu i Synowi, i Duchowi Świętemu.</div>
-        <div class="w">{a} / {b}</div>
+        <div class="w">{a}{b && ` / ${b}`}</div>
     </section>
 );
