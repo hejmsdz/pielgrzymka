864deb034b0ff55ea153ccf7138deeb351ae195e
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2022-06-27 20:08:14 +0200

    display standalone

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
                 start_url: `/`,
                 background_color: `#663399`,
                 theme_color: `#663399`,
-                display: `minimal-ui`,
+                display: `standalone`,
                 icon: "src/images/icon.png"
             }
         },
