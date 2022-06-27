fb2d2c6ddb95e195a2acbbc0b30a0295f53c4c9f
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2022-06-28 00:40:38 +0200

    fix toolbar

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -2,9 +2,9 @@ import IconDark from "../images/dark.svg";
 import IconLight from "../images/light.svg";
 import IconZoomIn from "../images/zoom_in.svg";
 import IconZoomOut from "../images/zoom_out.svg";
-import IconDone from "../images/done.svg";
-import IconOffline from "../images/offline.svg";
-import IconLoading from "../images/loading.svg";
+// import IconDone from "../images/done.svg";
+// import IconOffline from "../images/offline.svg";
+// import IconLoading from "../images/loading.svg";
 
 export default () => (
   <div className="toolbar">
