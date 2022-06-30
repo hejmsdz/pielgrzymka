dcc5415c14244e78528a98c939779929b5171386
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2022-06-30 23:25:25 +0200

    most songs for the Masses

--- src/components/breviary/psalm.jsx
+++ src/components/breviary/psalm.jsx
@@ -1,15 +1,15 @@
 import Antiphon from "./antiphon";
+import Tone from "./tone";
 import Multiline from "../multiline";
 
-export default ({ no, antiphon, children }) => (
+export default ({ no, antiphon, tone, children }) => (
   <section className="psalm">
+    {tone && <Tone no={tone} />}
     <h2>Psalm {no}</h2>
     <Antiphon>{antiphon}</Antiphon>
-
     {React.Children.map(children, (verse) => (
       <Multiline className="verse">{verse}</Multiline>
     ))}
-
     <Antiphon>{antiphon}</Antiphon>
   </section>
 );
